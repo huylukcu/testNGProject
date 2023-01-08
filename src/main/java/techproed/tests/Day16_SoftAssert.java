46b67708f63574bc335862116b2e54052199c606
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-08 12:20:10 -0600

    commit

---
+++ src/main/java/techproed/tests/Day16_SoftAssert.java
@@ -0,0 +1,24 @@
+package techproed.tests;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Day16_SoftAssert {
+    @Test
+    public void softSoftTest() {
+        SoftAssert softAssert = new SoftAssert();
+        System.out.println("Line 11 Code");
+        softAssert.assertEquals(3, 5);
+        System.out.println("Line 13 Code");
+        softAssert.assertAll();
+        /*
+        Dokuman uzerinde yapilan dogrulama islerine verification deniyor.
+        Kodu calistirdiktan sonra ortaya cikan sonuctan, beklenen netice aliniyor mu,
+        alinmiyor mu, bunun dogrulanmasina validation denir.
+        hard assertion to see the bug reason
+        check requirements ==> verification
+        check expected and actual match==>validation
+
+         */
+    }
+}
