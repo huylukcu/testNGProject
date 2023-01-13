fba9e902e689a36b3f2ef1d1260fd4da6d309365
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-12 22:03:59 -0600

    commit

--- src/main/java/techproed/tests/Day17_FirstConfigTest.java
+++ src/main/java/techproed/tests/Day17_FirstConfigTest.java
@@ -1,14 +1,22 @@
 package techproed.tests;
 
+import org.checkerframework.checker.units.qual.C;
 import org.testng.annotations.Test;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 public class Day17_FirstConfigTest {
-    //go to techproed homepage
     //assert title
     @Test
     public void firstConfigTest(){
+        //go to techproed homepage
+        // Driver.getDriver().get("https://www.techproeducation.com");
+        Driver.getDriver().get(ConfigReader.getProperty("url_prod_techproed"));
+        System.out.println(Driver.getDriver().getTitle());
+        //assert title
+        //assertEquals("Techpro Education | Online It Courses & Bootcamps",Driver.getDriver().getTitle());  Hard Coding
+        assertEquals(ConfigReader.getProperty("app_title"),Driver.getDriver().getTitle());
 
-
-}}
+    }}
--- src/main/java/techproed/utilities/Driver.java
+++ src/main/java/techproed/utilities/Driver.java
@@ -3,6 +3,9 @@ package techproed.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
@@ -12,8 +15,29 @@ public class Driver {
 
     public static WebDriver getDriver() {
         if (driver == null) {   //it is not be initilatize,I will create driver for data
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver(); //assign new driver
+            switch (ConfigReader.getProperty("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver=new EdgeDriver();
+                    break;
+            }
+            // WebDriverManager.chromedriver().setup();
+            //driver = new ChromeDriver(); //assign new driver
             //driver= WebDriverManager.chromedriver.create();
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));//specific element
@@ -26,7 +50,7 @@ public class Driver {
     public static void closeDriver() {
         //if driver is already being used(pointing an object)
         //then quit the driver
-        if (driver != null) {      //eger driver null degilse close et
+          if (driver != null) {      //eger driver null degilse close et
             driver.quit();
             driver = null;        //simdi null olmali,yenisini olusturmak icin
         }
