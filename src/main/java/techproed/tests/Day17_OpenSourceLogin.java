945883b158380e552ff6ee2fee6779196901ac7c
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-01-13 00:02:13 -0600

    commit

---
+++ src/main/java/techproed/tests/Day17_OpenSourceLogin.java
@@ -0,0 +1,34 @@
+package techproed.tests;
+
+import org.testng.annotations.Test;
+import techproed.pages.OpenSourcePage;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+
+public class Day17_OpenSourceLogin {
+/*
+    POM:
+    1. Locate the page objects in the pages package
+    -username input
+    -password input
+    -login button
+    Note: Use constructor to instantiate page object
+
+*/
+      @Test
+      public void loginTest() throws InterruptedException {
+    OpenSourcePage openSourcePage = new OpenSourcePage();
+
+    Driver.getDriver().get(ConfigReader.getProperty("open_source_URL"));Thread.sleep(1000);
+    openSourcePage.userName.sendKeys(ConfigReader.getProperty("open_source_admin_username"));
+
+    Thread.sleep(1000);
+    openSourcePage.password.sendKeys(ConfigReader.getProperty("open_source_admin_password"));
+
+    Thread.sleep(1000);
+    openSourcePage.logInSubmit.click();
+
+    Driver.closeDriver();
+
+}
+}
--- src/main/java/techproed/utilities/ConfigReader.java
+++ src/main/java/techproed/utilities/ConfigReader.java
@@ -4,16 +4,16 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class ConfigReader {
-    //This class reads the configuration.properties file
+    //This class reads the configuration.properties file.bu dosya sabit bir kere olustursan yeter
     //Create Properties instance
-    private static Properties properties;
+    private static Properties properties;//hemen properties dosyasi olussun diye static
     static {
         //path of the configuration file  //static block: it works before everything
         String path="configuration.properties";
         try {
-            //Opening configuration.properties file using FileInputStream
+            //Opening configuration.properties file using FileInputStream.dosyayi tanitma
             FileInputStream fileInputStream = new FileInputStream(path);
-            properties = new Properties();
+            properties = new Properties();//
             properties.load(fileInputStream);
             //close the file
             fileInputStream.close();
